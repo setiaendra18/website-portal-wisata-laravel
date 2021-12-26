e14a6e6416061026e334ae34896a477f5eb438a0
setiaendra18 <setiaendra18@gmail.com>
2021-12-26 09:21:09 +0700

    menu

--- resources/views/template/frontend/menu.blade.php
+++ resources/views/template/frontend/menu.blade.php
@@ -3,12 +3,12 @@
       <li><a href="index.html" class="active">Beranda</a></li>
       <li class="dropdown"><a href="#"><span>Destinasi Wisata</span> <i class="bi bi-chevron-down"></i></a>
         <ul>
-          <li><a href="about.html">Wisata Kuliner</a></li>
-          <li><a href="team.html">Wisata Sejarah & Budaya</a></li>
-          <li><a href="testimonials.html">Wisata Alam</a></li>
+          <li><a href="{{url('wisata-kuliner')}}">Wisata Kuliner</a></li>
+          <li><a href="{{url('wisata-sejarah-budaya')}}">Wisata Sejarah & Budaya</a></li>
+          <li><a href="{{url('wisata-alam')}}">Wisata Alam</a></li>
         </ul>
       </li>
-      <li><a href="services.html">Fasilitas Umum</a></li>
+      <li><a href="services.html">Event</a></li>
       <li><a href="portfolio.html">Penginapan</a></li>
       <li><a href="contact.html">Kontak</a></li>
      
